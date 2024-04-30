f22a71fcb289c603d98956e156f96c9cc1dd8d0c
Manudev <996manu@gmail.com>
2024-04-30 17:47:07 +0200

    preparando taller con vue

--- Otros/primer-proyecto-vue/src/App.vue
+++ Otros/primer-proyecto-vue/src/App.vue
@@ -1,47 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const taller = 'Taller la tuerca 🛠️'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ taller }}</h1>
+  <h3>Servicios disponibles</h3>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <hr />
+  <h3>Repuestos</h3>
+  <section></section>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+h1,
+h3,
+p,
+li {
+  font-family: monospace;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+section {
+  display: flex;
 }
 </style>
